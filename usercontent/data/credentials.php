076ba502ab8b7edc53bb5da593fc1f441c34bc4f
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2024-11-03 13:19:00 +1100

    feature: externalized credentials with disabled defaults
    Ensures that even upon accidental overwriting by a newer version the upload feature is disabled (username and password being empty strings) rathern than becoming the default username and password as in previous versions which would inadvertedly open default credentials access

---
+++ usercontent/data/credentials.php
@@ -0,0 +1,13 @@
+<?php
+
+// If you are using a static hosting or simply do not wish to use this server-based update feature 
+// and rather prefer uploading the checklist.json manually (via FTP), please set both 
+// the username and the password to "" (empty string) or delete this file completely
+
+// modify the user name between the quotes "..." to match your preferences
+// the user name needs to be surrounded by quotes, example: $username = "taxonomist"; 
+$username = ""; 
+
+// modify the password between the quotes "..." to a unique strong password, 
+// the password needs to be surrounded by quotes, example: $password = "somereallystrongpassword";
+$password = "";
