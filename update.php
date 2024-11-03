2fb9197a2c033dc2f9e40b69285d90fa3beb8569
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2024-11-03 13:19:44 +1100

    feature: import externalized credentials

--- update.php
+++ update.php
@@ -1,16 +1,14 @@
 <?php
 
-// If you do not wish to use this server-based update feature and are using a static hosting
-// please set both the username and the password to "" (empty string) or delete this file completely
+$credentials_file = ".usercontent/data/credentials.php";
 
-$username = "checklist"; // modify the user name between the quotes "..." to match your preferences, the user name needs to be surrounded by quotes
-$password = "N@tur5-gr8!"; // modify the password between the quotes "..." to a unique strong password, the password needs to be surrounded by quotes
-
-// ####################################################################################################################
-
-/////////////////////////////////////////
-// Do not modify the code below this line
-/////////////////////////////////////////
+if (file_exists($credentials_file)) {
+   include $credentials_file;
+}
+else {
+    echo jsonState("error", "upload disabled by administrator");
+    die;
+}
 
 if ($username == "" || $password == "") {
     echo jsonState("error", "upload disabled by administrator");
@@ -26,7 +24,6 @@ if (strcmp($_POST["username"], $username) == 0 && strcmp($_POST["password"], $pa
     try {
         $checklistDirectory = "./usercontent/data/";
         if (!is_dir($checklistDirectory)) {
-            // dir doesn't exist, make it
             mkdir($checklistDirectory);
         }
 
