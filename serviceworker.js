99a98703c047c3af60d6211c5eafc06c45bc52dd
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2024-11-23 16:19:47 +1100

    chore: update version number

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
 const checklistURL = "./usercontent/data/checklist.json";
 const checklistFileName = "checklist.json";
 
-let version = "2.3.2";
+let version = "2.3.3";
 
 let appCacheNameBase = "static";
 let appCacheName = appCacheNameBase + "-v" + version;
