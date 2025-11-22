5e2268912f64c87f63ae8058062662168e26c1ad
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2025-11-22 17:53:36 +1100

    refactor: do not generate sourcemap

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     base: './',
     build: {
         outDir: 'dist',
-        sourcemap: true,
     },
     define: {
         'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
